afaf4904a7b77b90792a6be18324856f4c03654f
Neil Walker <neil@wynded.co.uk>
2020-03-07 13:39:06 +0000

    Implement is_finished() and stop() for MPlayer

--- src/players.py
+++ src/players.py
@@ -5,15 +5,24 @@ class PlayerInterface(object):
     def __init__(self, exe, default_args):
         self.exe = exe
         self.default_args = default_args
+        self.child = None
 
     def is_finished(self):
-        raise NotImplementedError()
+        if self.child:
+            finished = self.child.poll() is not None
+            if finished:
+                self.child = None
+            return finished
+        else:
+            return True
 
     def play(self, filepath):
         raise NotImplementedError()
 
     def stop(self):
-        raise NotImplementedError()
+        if self.child:
+            self.child.kill()
+            self.child = None
 
 
 class MPlayer(PlayerInterface):
@@ -31,26 +40,12 @@ class Mpg123(PlayerInterface):
         super().__init__(exe, default_args)
         self.child = None
 
-    def is_finished(self):
-        if self.child:
-            finished = self.child.poll() is not None
-            if finished:
-                self.child = None
-            return finished
-        else:
-            return True
-
     def play(self, filepath):
         cmd = [self.exe] + self.default_args + [filepath]
         self.child = subprocess.Popen(cmd,
                                       stdin=subprocess.PIPE,
                                       stdout=subprocess.DEVNULL,
                                       stderr=subprocess.DEVNULL)
-
-    def stop(self):
-        if self.child:
-            self.child.kill()
-            self.child = None
 
 
 class OmxPlayer(PlayerInterface):
