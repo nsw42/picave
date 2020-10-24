e2ed9398922f3e6d034d1d6cc7f3923fb5d3091a
Neil Walker <neil@wynded.co.uk>
2020-10-24 15:33:03 +0100

    Generate and store a class hierarchy diagram

---
+++ docs/extract_class_diagram.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cd $(dirname $0)/..
+
+awk '
+BEGIN {print "digraph G {"}
+/^class.*(.*)/ {
+    classname = gensub(/\(.*/, "", "g", $2)
+    baseclass = gensub(/^.*\(/, "", "g", $2)
+    baseclass = gensub(/\).*/, "", "g", baseclass)
+    q = "\""
+    print q classname q " -> " q baseclass q
+}
+END {print "}"}
+' src/*.py > docs/class_diagram.dot
+
+dot -Tpng -O docs/class_diagram.dot
