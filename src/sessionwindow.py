26d4dd3d42a48e2603636623627033d5594f4b4c
Neil Walker <neil@wynded.co.uk>
2020-10-24 16:12:23 +0100

    Ensure we play videos when the window has already been realized

--- src/sessionwindow.py
+++ src/sessionwindow.py
@@ -19,16 +19,17 @@ class SessionWindow(PlayerWindowInterface):
                  feed_url: str):
         super().__init__(config, label)
         self.video_area = Gtk.DrawingArea()
-        self.video_area.set_size_request(1500, 0)  # set the width, allow height to sort itself out
+        # self.video_area.set_size_request(1500, 0)  # set the width, allow height to sort itself out
         self.video_area.connect("realize", self.on_realized)
         self.video_player = None
         self.video_file = None
         self.playing = False
+        self.realized = False
 
         self.interval_window = IntervalWindow(config, feed_url)
 
-        self.video_layout = Gtk.HBox()
-        self.video_layout.pack_start(self.video_area, expand=True, fill=True, padding=0)
+        self.video_layout = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.video_layout.pack_start(self.video_area, expand=True, fill=True, padding=100)
         self.video_layout.pack_start(self.interval_window, expand=True, fill=True, padding=0)  # TODO: Proper padding
 
     def add_windows_to_stack(self, stack, window_name_to_handler):
@@ -47,15 +48,13 @@ class SessionWindow(PlayerWindowInterface):
             self.set_player_window_darwin()
         else:
             self.set_player_window_x11()
-
-        self.video_player.set_mrl(self.video_file.as_uri())
-        self.video_player.play()
-        # self.playback_button.set_image(self.pause_image)
-        # self.is_player_active = True
+        self.realized = True
+        self.play_when_realized()
 
     def set_player_window_darwin(self):
         # https://gitlab.gnome.org/GNOME/pygobject/issues/112
         # and https://www.mail-archive.com/vlc-commits@videolan.org/msg55659.html
+        # and https://github.com/oaubert/python-vlc/blob/master/examples/gtkvlc.py
         window = self.video_area.get_window()
 
         getpointer = ctypes.pythonapi.PyCapsule_GetPointer
@@ -80,6 +79,15 @@ class SessionWindow(PlayerWindowInterface):
         self.video_file = video_file
         self.interval_window.play(video_id)
         GLib.timeout_add_seconds(1, self.monitor_for_end_of_video)
+        if self.realized:
+            self.play_when_realized()
+
+    def play_when_realized(self):
+        self.video_player.set_mrl(self.video_file.as_uri())
+        self.video_player.play()
+        self.video_player.video_set_scale(0)
+        # self.playback_button.set_image(self.pause_image)
+        # self.is_player_active = True
 
     def play_pause(self):
         assert self.video_player
