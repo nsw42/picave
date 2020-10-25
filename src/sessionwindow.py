a5f4a87295bf07d5e15293026e5cc7d72ff8d465
Neil Walker <neil@wynded.co.uk>
2020-10-25 12:37:51 +0000

    Simplify state management
    Wait until we know the file, the window and the window size before
    we start to play.

--- src/sessionwindow.py
+++ src/sessionwindow.py
@@ -54,25 +54,28 @@ class SessionWindow(PlayerWindowInterface):
         stack.add_named(self.video_layout, session_window_name)
         window_name_to_handler[session_window_name] = self.video_layout
 
+    def have_all_prerequisites_for_playing(self):
+        return (self.video_file is not None) and (self.realized) and (self.size_known)
+
     def on_draw(self, widget, context, data=None):
         context.set_source_rgb(0, 0, 0)
         context.paint()
 
     def on_realized(self, widget, data=None):
-        assert self.video_file  # play() must be called before this window is realised
-        assert self.playing
         self.realized = True
-        self.play_when_realized()
+        if self.have_all_prerequisites_for_playing():
+            self.start_playing()
 
     def on_size_changed(self, widget, allocation):
+        # This might be the final event allowing us to actually start playback,
+        # or it might be a size change when we're already playing
         assert self.video_file_width
-        if allocation.width > self.video_file_width:
-            # Don't attempt to scale up: the R-Pi isn't up to it
-            self.video_player.video_set_scale(1.0)
-        else:
-            # automatically scale down to fit the window
-            self.video_player.video_set_scale(0.0)
         self.size_known = True
+        if self.video_player:
+            self.set_video_scale(allocation)
+        else:
+            if self.have_all_prerequisites_for_playing():
+                self.start_playing()
 
     def set_player_window(self):
         logging.debug("set_player_window")
@@ -112,28 +115,35 @@ class SessionWindow(PlayerWindowInterface):
         self.video_file_width = get_video_width(video_file)
         self.interval_window.play(video_id)
         GLib.timeout_add_seconds(1, self.monitor_for_end_of_video)
-        if self.realized:
-            self.play_when_realized()
+        if self.have_all_prerequisites_for_playing():
+            self.start_playing()
 
-    def play_when_realized(self):
+    def start_playing(self):
         # pre-requisites for playing a video:
         # * what file to play (via play())
         # * what window to play into (via on_realized())
+        # * what size the window is (via on_size_changed())
         # play() gets called before on_realized() for the first video;
         # but on_realized() is only called once, whereas play() can be
         # called multiple times.
         # For the first video, on_realized() is soon followed by
         # on_size_changed(), which is where we then set the video scaling;
         # on_size_changed() is not expected for later videos.
+        assert self.video_player is None
         self.vlcInstance = vlc.Instance("--no-xlib")
         self.video_player = self.vlcInstance.media_player_new()
         self.video_player.set_mrl(self.video_file.as_uri())
         self.video_player.play()
         self.set_player_window()
-        if self.size_known:
-            # this is the 2nd (or later) video. Fake up a size event to
-            # set the video scaling.
-            self.on_size_changed(self.video_area, self.video_area.get_allocation())
+        self.set_video_scale(self.video_area.get_allocation())
+
+    def set_video_scale(self, video_area_allocation):
+        if video_area_allocation.width > self.video_file_width:
+            # Don't attempt to scale up: the R-Pi isn't up to it
+            self.video_player.video_set_scale(1.0)
+        else:
+            # automatically scale down to fit the window
+            self.video_player.video_set_scale(0.0)
 
     def play_pause(self):
         assert self.video_player
