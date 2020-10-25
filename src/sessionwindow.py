c9b3dfc05530246916dcab59fdcae634a697cd08
Neil Walker <neil@wynded.co.uk>
2020-10-25 10:52:58 +0000

    Don't scale up videos to fit a window

--- src/sessionwindow.py
+++ src/sessionwindow.py
@@ -1,5 +1,7 @@
 import ctypes
+import json
 import logging
+import subprocess
 import sys
 
 import vlc
@@ -13,6 +15,13 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gdk, GLib, Gtk  # noqa: E402 # need to call require_version before we can call this
 
 
+def get_video_width(filepath):
+    result = subprocess.run(['ffprobe', '-v', 'error', '-print_format', 'json', '-select_streams', 'v:0', '-show_entries', 'stream=width,height', str(filepath)],
+                            capture_output=True, text=True)
+    result = json.loads(result.stdout)
+    return result['streams'][0]['width']
+
+
 class SessionWindow(PlayerWindowInterface):
     def __init__(self,
                  config: Config,
@@ -22,19 +31,23 @@ class SessionWindow(PlayerWindowInterface):
         self.video_area = Gtk.DrawingArea()
         self.video_player = None
         self.video_file = None
+        self.video_file_width = None  # The natural size of the video
         self.playing = False
         self.realized = False
         self.vlcInstance = vlc.Instance("--no-xlib")
         self.video_player = self.vlcInstance.media_player_new()
 
         self.interval_window = IntervalWindow(config, feed_url)
+        self.interval_window.set_size_request(256, -1)
 
         self.video_layout = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.video_layout.set_homogeneous(False)
         self.video_layout.pack_start(self.video_area, expand=True, fill=True, padding=0)
-        self.video_layout.pack_start(self.interval_window, expand=True, fill=True, padding=0)
+        self.video_layout.pack_start(self.interval_window, expand=False, fill=False, padding=0)
 
-        self.video_area.connect("realize", self.on_realized)
         self.video_area.connect("draw", self.on_draw)
+        self.video_area.connect("realize", self.on_realized)
+        self.video_area.connect("size-allocate", self.on_size_changed)
 
     def add_windows_to_stack(self, stack, window_name_to_handler):
         session_window_name = "session_window"
@@ -52,6 +65,15 @@ class SessionWindow(PlayerWindowInterface):
         self.play_when_realized()
         self.set_player_window()
 
+    def on_size_changed(self, widget, allocation):
+        assert self.video_file_width
+        if allocation.width > self.video_file_width:
+            # Don't attempt to scale up: the R-Pi isn't up to it
+            self.video_player.video_set_scale(1.0)
+        else:
+            # automatically scale down to fit the window
+            self.video_player.video_set_scale(0.0)
+
     def set_player_window(self):
         logging.debug("set_player_window")
         if sys.platform == 'win32':
@@ -60,7 +82,6 @@ class SessionWindow(PlayerWindowInterface):
             self.set_player_window_darwin()
         else:
             self.set_player_window_x11()
-        self.video_player.video_set_scale(0)
 
     def set_player_window_darwin(self):
         # https://gitlab.gnome.org/GNOME/pygobject/issues/112
@@ -88,6 +109,7 @@ class SessionWindow(PlayerWindowInterface):
     def play(self, video_file, video_id):
         self.playing = True
         self.video_file = video_file
+        self.video_file_width = get_video_width(video_file)
         self.interval_window.play(video_id)
         GLib.timeout_add_seconds(1, self.monitor_for_end_of_video)
         if self.realized:
