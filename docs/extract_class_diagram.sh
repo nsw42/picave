b74d1dffaa763171d2b01162db9c26197b0b27b5
Neil Walker <neil@wynded.co.uk>
2020-11-15 20:18:05 +0000

    Shade external base classes

--- docs/extract_class_diagram.sh
+++ docs/extract_class_diagram.sh
@@ -11,6 +11,15 @@ BEGIN {print "digraph G {"}
     if (baseclass != "object") {
       q = "\""
       print q classname q " -> " q baseclass q
+
+      if ((baseclass == "Exception") || (substr(baseclass, 1, 3) == "Gtk") || (substr(baseclass, 1, 6) == "ctypes")) {
+        fillcolour="#d0d0d0";
+      } else {
+        fillcolour="";
+      }
+      if (fillcolour != "") {
+        print q baseclass q " [ fillcolor=" q fillcolour q ", style=" q "filled" q " ]"
+      }
     }
 }
 END {print "}"}
