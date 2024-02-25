445903633437c0c60b05ee8033130fde4a275495
Neil Walker <neil@wynded.co.uk>
2024-02-25 12:30:44 +0000

    Try to find a default video player

--- profile/default.go
+++ profile/default.go
@@ -22,18 +22,34 @@ func defaultProfileExecutables() map[string]*Executable {
 
 func defaultProfilePlayers(executables map[string]*Executable) map[string]*FiletypePlayerOptions {
 	rtn := map[string]*FiletypePlayerOptions{}
-	for _, filetype := range supportedFiletypes {
-		rtn[filetype] = nil
-	}
 	rtn[".mp3"] = defaultMusicPlayer(executables)
+	rtn[".mk4"] = defaultVideoPlayer(executables)
+	rtn[".mkv"] = defaultVideoPlayer(executables)
 	return rtn
 }
 
 func defaultMusicPlayer(executables map[string]*Executable) *FiletypePlayerOptions {
 	// TODO: It would be nice to generalise this - but attempting to reference players.MusicPlayerLookup results in an import cycle
-	mpg123 := executables["mpg123"]
-	if mpg123.ExePath() != "" {
-		return &FiletypePlayerOptions{"mpg123", []string{}, Margins{}}
+	return tryFindExe(executables, "mpg123")
+}
+
+func defaultVideoPlayer(executables map[string]*Executable) *FiletypePlayerOptions {
+	// omxplayer first, because that's the one that's got hardware support on the RPi
+	// TODO: It would be nice to generalise this - but attempting to reference players.MusicPlayerLookup results in an import cycle
+	player := tryFindExe(executables, "omxplayer")
+	if player == nil {
+		player = tryFindExe(executables, "mplayer")
+	}
+	if player == nil {
+		player = tryFindExe(executables, "mpv")
+	}
+	return player
+}
+
+func tryFindExe(executables map[string]*Executable, exename string) *FiletypePlayerOptions {
+	exe := executables[exename]
+	if exe.ExePath() != "" {
+		return &FiletypePlayerOptions{exename, []string{}, Margins{}}
 	}
 	return nil
 }
