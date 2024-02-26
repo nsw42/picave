5c29c7194d590e05317dcd59f972809246189bc6
Neil Walker <neil@wynded.co.uk>
2024-02-26 22:44:33 +0000

    No separate build script is required
    `go build .` is sufficient, so remove the build_release.sh,
    and put everything into `build.sh`.

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #! /bin/sh
 
-DEST=$1
-if [ -z "$DEST" ]; then
-  DEST=picave
-fi
+set -e
+
+cross_compile() {
+  echo ======================== Building with $1 ========================
+  builder=$1
+  destdir=$2
+  docker run -it --rm -v ./:/go/src $MAP_PKG_DIR_ARGS -v $destdir:/go/output -w /go/src "$builder" sh -c '[ -f /etc/profile.d/go_cross.sh ] && . /etc/profile.d/go_cross.sh; go mod tidy; go build -v -ldflags "-s -w" -o /go/output/picave .'
+}
+
+cross_compile gotk-cross-builder-alpine3.19-armhf  ./dist/alpine3.19-armhf
+cross_compile gotk-cross-builder-alpine3.19-arm64  ./dist/alpine3.19-arm64
+cross_compile gotk-cross-builder-bookworm-armhf  ./dist/debian-bookworm-armhf
+cross_compile gotk-cross-builder-bookworm-arm64  ./dist/debian-bookworm-arm64
 
 go mod tidy
-go build -o "$DEST" .
+mkdir -p dist/macOS
+go build -o dist/macOS/picave .
