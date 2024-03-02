f3a7a479790eeb89a53c23a484cd8d2f5a72d325
Neil Walker <neil@wynded.co.uk>
2024-03-02 14:23:26 +0000

    Make the libvlc player build, though it still doesn't work
    Looks like versionitis between the different versions of GTK: libvlc
    relies on gtk3, whereas picave is using gtk4

--- src/players/vlc.go
+++ src/players/vlc.go
@@ -2,18 +2,34 @@
 
 package players
 
+/*
+#cgo CFLAGS: -x objective-c
+#cgo CFLAGS: -I/Applications/VLC.app/Contents/MacOS/include
+#cgo LDFLAGS: -L/Applications/VLC.app/Contents/MacOS/lib
+#cgo pkg-config: gdk-3.0
+#include <AppKit/AppKit.h>
+#include <gdk/gdk.h>
+GDK_AVAILABLE_IN_ALL NSView* gdk_quartz_window_get_nsview(GdkWindow *window);
+*/
+import "C"
+
+// Runtime: export DYLD_LIBRARY_PATH=/lib:/Applications/VLC.app/Contents/MacOS/lib
+
 import (
+	"fmt"
 	"log"
 	"nsw42/picave/profile"
 	"os/exec"
+	"unsafe"
 
 	vlc "github.com/adrg/libvlc-go/v3"
+	"github.com/diamondburned/gotk4/pkg/gdk/v4"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
 
 func init() {
 	if err := vlc.Init(); err != nil {
-		log.Fatal(err)
+		log.Fatal("VLC:", err)
 	}
 	// vlc.Release?
 }
@@ -25,24 +41,39 @@ type VlcPlayer struct {
 	Vlc     *vlc.Player
 }
 
-func NewVlcPlayer(profile *profile.Profile) Player {
+func NewVlcPlayer(profile *profile.Profile, options *profile.FiletypePlayerOptions) VideoPlayer {
 	return &VlcPlayer{PlayerNotStarted, profile, nil, nil}
 }
 
 func init() {
-	registerPlayer("vlc", NewVlcPlayer)
+	registerVideoPlayer("vlc", NewVlcPlayer)
 }
 
 func (player *VlcPlayer) PlayerState() PlayerState {
 	return player.State
 }
 
-func (player *VlcPlayer) Play(file string) {
-	// TODO
-}
-
-func (player *VlcPlayer) PlayVideo(file string, drawingAreea *gtk.DrawingArea) {
-	go player.launch(file, drawingAreea)
+func (player *VlcPlayer) Play(file string, parent *gtk.Widget) *gtk.Widget {
+	drawingArea := gtk.NewDrawingArea()
+	drawingArea.SetParent(parent)
+	drawingArea.ConnectRealize(func() {
+		surface := drawingArea.Native().Surface().(*gdk.Surface)
+		fmt.Printf("Surface: %p\n", surface)
+		if surface != nil {
+			object := surface.Object
+			fmt.Println("Object:", object)
+			window := (*C.GdkWindow)(unsafe.Pointer(surface.Object))
+			fmt.Printf("Window: %p\n", window)
+			if window != nil {
+				handle := unsafe.Pointer(C.gdk_quartz_window_get_nsview(window))
+				if uintptr(handle) != 0 {
+					player.Vlc.SetNSObject(uintptr(handle))
+				}
+			}
+		}
+	})
+	go player.launch(file)
+	return &drawingArea.Widget
 }
 
 func (player *VlcPlayer) PlayPause() {
@@ -57,8 +88,7 @@ func (player *VlcPlayer) Stop() {
 	}
 }
 
-func (player *VlcPlayer) launch(file string, drawingArea *gtk.DrawingArea) {
-	surface := drawingArea.Native().Surface()
+func (player *VlcPlayer) launch(file string) {
 	vlcPlayer, err := vlc.NewPlayer()
 	if err != nil {
 		log.Println("Unable to launch VLC")
