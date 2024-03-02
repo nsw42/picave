1984cef190da2af0e6cb2a690f3e3dbecb4d80e4
Neil Walker <neil@wynded.co.uk>
2024-03-02 11:20:12 +0000

    Move all the .go source into a src subdirectory, for a tidier directory structure

--- build.sh
+++ build.sh
@@ -4,17 +4,20 @@ set -e
 
 if [ "$1" = "--release" ]; then
   echo Release build
+  cd src
   go clean -r -cache -testcache -modcache -fuzzcache
-  VER=$(git describe)
-  DATE=$(date +%Y-%m-%d)
-  echo "$VER ($DATE)" > version.txt
+  cd ..
 fi
+
+VER=$(git describe)
+DATE=$(date +%Y-%m-%d)
+echo "$VER ($DATE)" > version.txt
 
 cross_compile() {
   echo ======================== Building with $1 ========================
   builder=$1
   destdir=$2
-  docker run -it --rm -v ./:/go/src $MAP_PKG_DIR_ARGS -v $destdir:/go/output -w /go/src "$builder" sh -c '[ -f /etc/profile.d/go_cross.sh ] && . /etc/profile.d/go_cross.sh; go mod tidy; go build -v -ldflags "-s -w" -o /go/output/picave .'
+  docker run -it --rm -v ./src:/go/src $MAP_PKG_DIR_ARGS -v $destdir:/go/output -w /go/src "$builder" sh -c '[ -f /etc/profile.d/go_cross.sh ] && . /etc/profile.d/go_cross.sh; go mod tidy; go build -v -ldflags "-s -w" -o /go/output/picave .'
 }
 
 cross_compile gotk-cross-builder-alpine3.19-armhf  ./dist/alpine3.19-armhf
@@ -22,6 +25,7 @@ cross_compile gotk-cross-builder-alpine3.19-arm64  ./dist/alpine3.19-arm64
 cross_compile gotk-cross-builder-bookworm-armhf  ./dist/debian-bookworm-armhf
 cross_compile gotk-cross-builder-bookworm-arm64  ./dist/debian-bookworm-arm64
 
+cd src
 go mod tidy
-mkdir -p dist/macOS
-go build -o dist/macOS/picave .
+mkdir -p ../dist/macOS
+go build -o ../dist/macOS/picave .
